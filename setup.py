bda8dca05ee84a7a64e51d35d84e37446fc2d1ec
adriantpaez <adriantpaez@gmail.com>
2022-10-30 17:03:04 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import sys
 from jinja2 import Template
 
 if __name__ == "__main__":
-    labels = sys.argv[1]
-    ips = sys.argv[2]
+    labels = sys.argv[1].split(',')
+    ips = sys.argv[2].split(',')
 
     with open('./prometheus.yml.j2') as f:
         tmpl:Template = Template(f.read())
     nodes_data = zip(labels, ips)
-    prometheus_config = tmpl.render(nodes_data)
+    prometheus_config = tmpl.render(nodes_data=nodes_data)
     print(prometheus_config)
