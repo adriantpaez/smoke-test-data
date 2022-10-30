92d68890348563562af8230dc4638d184451463b
adriantpaez <adriantpaez@gmail.com>
2022-10-30 18:49:34 -0400

    Update prometheus setup

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from jinja2 import Template
 if __name__ == "__main__":
     labels = sys.argv[1].split(',')
     ips = sys.argv[2].split(',')
-    cl_consensus = sys.argv[3].split(',')
+    execution_clients = sys.argv[3].split(',')
+    beacon_clients = sys.argv[4].split(',')
 
     with open('./prometheus.yml.j2') as f:
         tmpl:Template = Template(f.read())
-    nodes_data = zip(labels, ips, cl_consensus)
+    nodes_data = zip(labels, ips, execution_clients, beacon_clients)
     prometheus_config = tmpl.render(nodes_data=nodes_data)
     print(prometheus_config)
